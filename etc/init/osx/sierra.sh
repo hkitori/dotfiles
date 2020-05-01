aef38f211c0677ef61a75d7271d10ba7dc29ee50
ytkibk <ytkibk@users.noreply.github.com>
2020-05-02 08:29:28 +0900

    clone dein.vim in .vimrc

--- etc/init/osx/sierra.sh
+++ etc/init/osx/sierra.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # setup vim
-DEIN_DIR="${HOME}/.cache/dein/"
-mkdir -p ${DEIN_DIR}
-cd ${DEIN_DIR}
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./installer.sh ${DEIN_DIR}
-cd -
+#DEIN_DIR="${HOME}/.cache/dein"
+#mkdir -p ${DEIN_DIR}
+#cd ${DEIN_DIR}
+#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+#sh ./installer.sh ${DEIN_DIR}
+#cd -
+

100c40d55f07dff417726bc5e1bbc34dc10a1eba
ytkibk <ytkibk@users.noreply.github.com>
2020-05-03 17:18:12 +0900

    update raspi4/init.sh

--- etc/init/raspi4/init.sh
+++ etc/init/raspi4/init.sh
@@ -9,12 +9,18 @@ sudo raspi-config nonint do_change_timezone Asia/Tokyo
 sudo raspi-config nonint do_change_locale en_US.UTF-8
 
 # hdmi_group = 1 : CEA
-# hdmi_mode = 4  : 1280x720 @ 60Hz, progressive
-sudo raspi-config nonint do_resolution 1 4
+# hdmi_mode = 16  : 1920x1080 @ 60Hz, progressive
+sudo raspi-config nonint do_resolution 1 16
+# hdmi_mode = 97  : 3840x2160 @ 60Hz, progressive
+# 97 is Raspberry Pi 4 Only. To use this,
+# hdmi_enable_4kp60=1 must be set in /boot/config.txt.
+
 # add gpu_mem=512
 sudo raspi-config nonint do_memory_split 512
 # comment out disable_overscan
 sudo raspi-config nonint do_overscan 0
+# turn off blanking
+sudo raspi-config nonint do_blanking 1
 
 
 sudo apt update

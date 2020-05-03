80331e121dabb66c11948553817560999f76c3a8
ytkibk <ytkibk@users.noreply.github.com>
2020-05-03 17:24:44 +0900

    update raspi4/init.sh

--- etc/init/raspi4/init.sh
+++ etc/init/raspi4/init.sh
@@ -30,6 +30,10 @@ sudo apt -y upgrade vim
 # google JP fonts
 sudo apt -y install fonts-noto
 # input method for JP
+#   You need also the following manual setting with GUI:
+#     Menu -> Preferences -> Ibus Preferences
+#     -> General -> Use custom font: Noto Sans CJK JP Regular
+#     font size: 10 or 12
 sudo apt -y install ibus-mozc
 
 

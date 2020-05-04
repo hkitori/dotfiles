f4412ae1cc197b00fb3ac4edb531c496953e5e6a
ytkibk <ytkibk@users.noreply.github.com>
2020-05-04 21:55:34 +0900

    update retropie scripts

--- specific/raspi4/bin/pair_joycon_blue.sh
+++ specific/raspi4/bin/pair_joycon_blue.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eux
 
-./bluetooth.sh 98:B6:E9:84:98:48 # Joy-Con (L)
+bluetooth.sh 98:B6:E9:84:98:48 # Joy-Con (L)
 
--- specific/raspi4/bin/pair_joycon_red.sh
+++ specific/raspi4/bin/pair_joycon_red.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -eux
 
-./bluetooth.sh 98:B6:E9:8A:56:7D # Joy-Con (R)
+bluetooth.sh 98:B6:E9:8A:56:7D # Joy-Con (R)
 

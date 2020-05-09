04cc8afac041d766a8c7723c17fd8d9e3b93b15d
ytkibk <ytkibk@users.noreply.github.com>
2020-05-09 15:32:20 +0900

    add cd top

--- .zsh/20_keybinds.zsh
+++ .zsh/20_keybinds.zsh
@@ -2,7 +2,7 @@
 # ctrl-a, ctrl-e
 bindkey -e
 
-# peco history selection (ctrl + r)
+# ヒストリをpecoで選択し実行する (ctrl + r)
 function _peco-select-history() {
     # historyを番号なし、逆順、最初から表示。
     # 順番を保持して重複を削除。
@@ -15,7 +15,7 @@ function _peco-select-history() {
 zle -N _peco-select-history
 bindkey '^R' _peco-select-history
 
-# start/stop tmux toggle (ctrl + ])
+# tmuxの起動・停止 (ctrl + ])
 _start_tmux_if_stopped() {
     if [[ -n $TMUX ]]; then
         # tmux is running
@@ -32,4 +32,20 @@ _start_tmux_if_stopped() {
 }
 zle -N _start_tmux_if_stopped
 bindkey '^]' _start_tmux_if_stopped
+
+# gitのトップディレクトリに移動 (ctrl + t)
+_cd_top() {
+    # git grepが使えるかどうか調べる
+    if is_in_git; then
+        # git内なので使える
+        builtin cd `git rev-parse --show-toplevel`
+    else
+        # git外なので使えない
+        # その場合はホームに移動
+        builtin cd ~/
+    fi
+    zle accept-line
+}
+zle -N _cd_top
+bindkey '^T' _cd_top
 

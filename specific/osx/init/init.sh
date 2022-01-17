4a60f3e39d9639cf0997761d99e0daa6194ec1d0
hkitori <hkitori@users.noreply.github.com>
2022-01-17 12:10:03 +0900

    fix init.sh for mac

--- specific/osx/init/init.sh
+++ specific/osx/init/init.sh
@@ -17,10 +17,30 @@ mkdir -p ~/.vim/colors
 git clone https://github.com/tomasr/molokai ~/.vim/colors/
 mv ~/.vim/colors/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
 
-brew install global # for gtags
+# install terminal app
+brew cask install iTerm2
+
+# Powerline Fontsのインストール
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Nerd Fontsのインストール
+git clone --branch=master --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+cd ..
+rm -fr nerd-fonts
+# この後、手動でiterm1のフォントを"Sauce Code Pro Nerd Font Complete"に設定する
+
+# for gtags
+brew install global
 
 # for tmux
 brew install reattach-to-user-namespace
 
 # mac command like watch or inotifywait
 brew install fswatch
+
